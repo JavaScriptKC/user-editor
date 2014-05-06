6a5ba4e948537d66e4731be94f710c31a155933f
Joe Andaverde <joe@andaverde.net>
2014-05-06 14:43:23 -0500

    Added user details page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function (app, db) {
    app.get('/', function (req, res) {
       db.collection('nodelabs-users', function (err, collection) {
       collection.find().toArray(function (err, users) {
             res.render('index.jade', { users: users });
+         });
+      });
+   });
+
+   app.get('/user/:id', function (req, res) {
+      var id = req.params.id;
+      db.collection('nodelabs-users', function (err, collection) {
+         collection.findOne({ _id: ObjectID(id) }, function (err, user) {
+            res.render('user.jade', { user: user });
          });
       });
    });
